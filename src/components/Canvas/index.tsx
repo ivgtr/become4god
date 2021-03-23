c58b13d78ee996e7fcbdc1cc6d262d816c13ba55
ivgtr <gear.1112t.o@gmail.com>
2021-03-23 22:03:40 +0900

    :bug: fix: base64の画像がsafari系で表示されないので戻す

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useRef } from 'react'
-import domtoimage from 'dom-to-image'
 import { Circle } from '../Circle'
 import classes from './Canvas.module.css'
 
@@ -12,17 +11,32 @@ export const Canvas: React.VFC<{ colors: string[][] }> = (props) => {
   }, [props.colors])
 
   useEffect(() => {
+    console.log(colors.length)
     if (!colors.length) return
+    console.log('null')
     const wrapperElement = wrapperRef.current
     if (!wrapperElement) return
+    const sr =
+      wrapperElement.shadowRoot ||
+      wrapperElement.attachShadow({
+        mode: 'open'
+      })
+    while (sr.firstChild) {
+      sr.firstChild.remove()
+    }
 
     const maxColorValue = colors.length
     const size = 100 / maxColorValue
 
-    const df = document.createElement('div')
-    df.style.height = '640px'
-    df.style.width = '640px'
-    df.style.position = 'relative'
+    const df = document.createDocumentFragment()
+    const spacer = document.createElement('div')
+    spacer.style.width = '100%'
+    spacer.style.height = '100%'
+    spacer.style.left = '0'
+    spacer.style.top = '0'
+    spacer.style.margin = '0 auto'
+    spacer.style.position = 'absolute'
+    df.append(spacer)
 
     const cell = document.createElement('div')
     cell.style.position = 'absolute'
@@ -41,23 +55,10 @@ export const Canvas: React.VFC<{ colors: string[][] }> = (props) => {
         cell.style.left = `${size * index}%`
         cell.style.backgroundColor = color
         const c = cell.cloneNode(false)
-        df.append(c)
+        spacer.append(c)
       })
     })
-
-    domtoimage
-      .toPng(df, { width: 640, height: 640 })
-      .then(function (dataUrl) {
-        const img = new Image()
-        img.src = dataUrl
-        img.style.width = '100%'
-        img.alt = `${new Date()} God`
-        img.id = 'canvas'
-        wrapperElement.append(img)
-      })
-      .catch(function (error) {
-        console.error('oops, something went wrong!', error)
-      })
+    sr.append(df)
   }, [colors])
 
   return (
